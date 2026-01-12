8b5bf7555b4c0833ea75b19d0b2807c588a51d60
Mausam100 <smausam104@gmail.com>
2026-01-12 18:20:44 +0530

    chnage laoyout

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
 
   return (
     <div className="w-full h-screen bg-[#E8E9EC] flex items-center justify-center relative">
-      <div className="absolute left-60 text-5xl text-blue-900 z-10">
+      <div className="absolute left-60 text-5xl text-nowrap w-96 h-80 hover:text-white flex items-center justify-center  text-blue-900 z-10">
         NATURE'S PUREST
       </div>
       <BottleHover normalImage={normalImage} hoverImage={hoverImage} />
-      <div className="absolute right-30 text-5xl text-blue-900">
+      <div className="absolute w-96 h-80 hover:text-white right-40 text-5xl flex items-center justify-center text-nowrap text-blue-900 ">
         FORM OF HYDRATION
       </div>
     </div>
